22752e20c4bdbb9d331f4bee3ca386b9ed125d70
Voldart <74236937+bohdandash@users.noreply.github.com>
2023-05-13 09:39:56 +0300

    Update&Cleaning StatementPrinterTests.cs

--- csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
+++ csharp/TheatricalPlayersRefactoringKata.Tests/StatementPrinterTests.cs
@@ -10,35 +10,46 @@ namespace TheatricalPlayersRefactoringKata.Tests
     {
         [Fact]
         [UseReporter(typeof(DiffReporter))]
-        public void test_statement_example()
+        public void TestStatementExample()
         {
+            // Arrange
             var plays = new Dictionary<string, Play>();
             plays.Add("hamlet", new Play("Hamlet", "tragedy"));
             plays.Add("as-like", new Play("As You Like It", "comedy"));
             plays.Add("othello", new Play("Othello", "tragedy"));
 
-            Invoice invoice = new Invoice("BigCo", new List<Performance>{new Performance("hamlet", 55),
-                new Performance("as-like", 35),
-                new Performance("othello", 40)});
-            
-            StatementPrinter statementPrinter = new StatementPrinter();
+            var invoice = new Invoice("BigCo", new List<Performance>{
+            new Performance("hamlet", 55),
+            new Performance("as-like", 35),
+            new Performance("othello", 40)});
+
+            var statementPrinter = new StatementPrinter();
+
+            // Act
             var result = statementPrinter.Print(invoice, plays);
 
+            // Assert
             Approvals.Verify(result);
         }
+
         [Fact]
-        public void test_statement_with_new_play_types()
+        public void TestStatementWithNewPlayTypes()
         {
+            // Arrange
             var plays = new Dictionary<string, Play>();
             plays.Add("henry-v", new Play("Henry V", "history"));
             plays.Add("as-like", new Play("As You Like It", "pastoral"));
 
-            Invoice invoice = new Invoice("BigCoII", new List<Performance>{new Performance("henry-v", 53),
-                new Performance("as-like", 55)});
-            
-            StatementPrinter statementPrinter = new StatementPrinter();
+            var invoice = new Invoice("BigCoII", new List<Performance>{
+            new Performance("henry-v", 53),
+            new Performance("as-like", 55)});
 
-            Assert.Throws<Exception>(() => statementPrinter.Print(invoice, plays));
+            var statementPrinter = new StatementPrinter();
+
+            // Act and Assert
+            var ex = Assert.Throws<Exception>(() => statementPrinter.Print(invoice, plays));
+            Assert.Equal("unknown type: history", ex.Message);
         }
     }
+
 }
